788c2a69fb5b74200919f84ba953f0afec31b5c4
Dmitry Filonenko <feelll@bigmir.net>
2018-08-27 12:20:28 +0300

    First version have done

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SemaUpd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] argArr = Environment.GetCommandLineArgs();
+                string destPath = argArr[1];
+                string sourcePath = @"x:\utils\Semaphore_new\0 Sema.exe";
+
+                Process[] processes = Process.GetProcessesByName("Sema");
+                processes[0].CloseMainWindow();
+
+                Thread.Sleep(1000);
+
+                File.Copy(sourcePath, destPath, true);
+
+                Process proc = new Process();
+                string pathAsArg = destPath;
+                if (destPath.Contains(" "))
+                {
+                    pathAsArg = "\"" + pathAsArg + "\""; 
+                }
+                proc.StartInfo.Arguments = "destPath";
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.FileName = pathAsArg;
+                proc.Start();                
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show("Возникла ошибка при обновлении Семы. " + Environment.NewLine + "Похоже, что нет запущеного приложения Семы." + Environment.NewLine + ex.Message, "Ошибка при обновлении", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Возникла ошибка при обновлении Семы. " + Environment.NewLine + ex.Message, "Ошибка при обновлении", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
